32d9662a8435c75bb2d92634bf4fa0f9c008ce61
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-06-25 20:19:49 -0400

    updates

--- src/app/utils/firebaseUtils.js
+++ src/app/utils/firebaseUtils.js
@@ -1,9 +1,34 @@
-import { collection, getDocs } from "firebase/firestore"; 
+import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 
-async function getAllDocuments(db, collectionName){
-    const querySnapshot = await getDocs(collection(db, collectionName));
+/**
+ * Generic that gets all documents from a firestore database and returns an array of objects
+ * @param {database} db
+ * @param {string} collectionName
+ * Utility Function that gets all documents from a firestore database and returns an array of objects
+ * @param {database instance} db An instance of a Cloud Firestore Database
+ * @param {string} collectionName The name of a Firestore db collection
+ * @returns {array}
+ * @returns an array of objects
+ */
+@@ -20,4 +20,19 @@ async function getAllDocuments(db, collectionName) {
+  return documents;
+}
 
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
+export { getAllDocuments };
+/**
+ * Utility Function that adds a document to a Google Cloud Firestore Database
+ * @param {database instance} db An instance of a Cloud Firestore Database
+ * @param {string} collectionName The name of a Firestore db collection
+ * @param {object} data An object representing a collection document
+ */
+async function addDocument(db, collectionName, data) {
+  try {
+    const docRef = await addDoc(collection(db, collectionName), data);
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+export { getAllDocuments, addDocument };
